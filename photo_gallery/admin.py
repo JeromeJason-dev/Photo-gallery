c3b20a6d40f58eb5fca1865a3cd2eb6f500b868e
JeromeJason <jasonnem048@gmail.com>
2026-09-04 16:29:20 +0300

    feat: for database records management

--- photo_gallery/admin.py
+++ photo_gallery/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Like, Photo, Profile, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ("title", "uploaded_by", "created_at", "like_count", "dislike_count")
+    list_filter = ("tags", "created_at")
+    search_fields = ("title", "description")
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("user", "photo", "value", "created_at")
+    list_filter = ("value",)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "bio")
