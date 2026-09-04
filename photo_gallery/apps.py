81973e9347bce9fb2f7ab81da1dbbe6b36306d1e
JeromeJason <jasonnem048@gmail.com>
2026-09-04 16:30:48 +0300

    feat: handle startup logic

--- photo_gallery/apps.py
+++ photo_gallery/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class PhotoGalleryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'photo_gallery'
+
+    def ready(self):
+        import photo_gallery.signals  # noqa: F401
